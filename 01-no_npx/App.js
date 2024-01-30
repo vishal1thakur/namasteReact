f4a748ca1c73c607408c03822dc35257401a8d88
vishal1thakur <interaction.vishal@gmail.com>
2024-01-31 02:16:38 +0530

    fundamentals setup + jsx

--- 01-no_npx/App.js
+++ 01-no_npx/App.js
@@ -1,4 +1,9 @@
-// How react creates elements
+import React from "react";
+import ReactDOM from "react-dom";
+
+// How react creates elements:
+
+// React.createElement => React Element-JS Object => HTMLElement(render)
 
 // heading is an object
 const heading = React.createElement(
@@ -6,6 +11,7 @@ const heading = React.createElement(
     { id: "heading" },
     "Hello World from React!"
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // object is converted to HTML by render
@@ -21,12 +27,12 @@ const parent = React.createElement(
         { id: "child" },
         // multiple children with array
         [
-            React.createElement("h1", {}, "I am an h1 tag"),
+            React.createElement("h1", {}, "I am an h111 tag"),
             React.createElement("h2", {}, "I am an h2 tag also"),
         ]
     )
 );
 
-root.render(parent);
-
 // This gets chaotic very fast, thats why we use JSX
+
+root.render(parent);
